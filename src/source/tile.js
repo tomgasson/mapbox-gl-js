8f11b79d6a7ff48e4d616e9063b1a724484193dc
Molly Lloyd <molly@mapbox.com>
2017-07-27 15:01:30 -0700

    Add algorithm to generate tilemasks
    fix bugs in the masking algorithm
    
    perform masking on raster tile loads
    
    clean up

--- src/source/tile.js
+++ src/source/tile.js
@@ -314,6 +314,10 @@ class Tile {
         }
     }
 
+    setMask(mask: any) {
+        console.log(mask);
+    }
+
     hasData() {
         return this.state === 'loaded' || this.state === 'reloading' || this.state === 'expired';
     }
